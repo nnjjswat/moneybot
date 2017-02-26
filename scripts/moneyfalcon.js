c017667c4c9654e70d3a1c9c3003d04aa63bc8e7
currentsea <currentsea@currentsea.io>
2017-02-25 17:23:41 -0800

    update flow

--- scripts/moneyfalcon.js
+++ scripts/moneyfalcon.js
@@ -38,7 +38,7 @@ var resetlosscountonwin = false;
 var winningstreakbetincrement = 1; 
 var abortwhenmedianfails = 0; // =0 to only abort failed streaks with maxlosingstreakcutoff counter, set to =1 to abort failed streaks when data model median6 > median6upperlimit as configured in the line above
 var abortonfailedentrycriteria = 0; // =0 to only abort failed streaks with maxlosingstreakcutoff counter, set to =1 to abort failed streaks when entrycriteria > last bust as configured above
-var botpriority = 1; 	// =1 to be first priority bot, =2 second priority bot, =3 third priority bot strategy to execute a bet when running on same account
+var botpriority = 1;    // =1 to be first priority bot, =2 second priority bot, =3 third priority bot strategy to execute a bet when running on same account
 
 var gamehistory = {}; 
 // gamehistory['0'] = 197; 
@@ -49,14 +49,14 @@ var gamehistory = {};
 // gamehistory['5'] = 197; 
 // gamehistory['6'] = 197; 
 var gameresults = []; 
-gameresults.push(197);
-gameresults.push(197);
-gameresults.push(197);
-gameresults.push(197);
-gameresults.push(197);
-gameresults.push(197);
-var median6 = median(gameresults); 
-console.log('starting median: ' + median6); 
+// gameresults.push(197);
+// gameresults.push(197);
+// gameresults.push(197);
+// gameresults.push(197);
+// gameresults.push(197);
+// gameresults.push(197);
+var median6 = 197; 
+console.log('starting median over last 6 games: ' + median6); 
 
 var grosswinnings = 0; 
 var grosslosses = 0; 
@@ -72,14 +72,40 @@ var moneybotconfirmations = 0;
 
 var gameselapsed = 0; 
 
-engine.on('game_starting', play_game); 
+
+/** BEGIN ENGINE INSTRUCTIONS */ 
+engine.on('game_starting', start_game); 
+
+engine.on('game_started', play_game); 
 
 engine.on('game_crash', finish_game); 
 
-// engine.on('game_started', play_game_now); 
 
 // engine.on('cashed_out', process_player_cashout); 
+/** END ENGINE INSTRUCTIONS */ 
 
+
+/** BEGIN ENGINE LOGIC */ 
+function start_game(gamedata) { 
+    log_pre_game_data(gamedata); 
+    calculate_net_profits(); 
+    console.log("game # " + gameselapsed + " is starting..."); 
+    calculate_win_rate(); 
+    summarize(); 
+}
+
+
+function play_game(gamedata) { 
+    console.log("we are now playing the game"); 
+}
+
+function finish_game(gamedata) { 
+    console.log("game finished"); 
+    gameselapsed++; 
+} 
+/** END ENGINE LOGIC */ 
+
+/** HELPERS */ 
 function median(values) {
     values.sort( function(a,b) {return a - b;} );
     var half = Math.floor(values.length/2);
@@ -88,22 +114,69 @@ function median(values) {
         median = values[half];
     }
     else { 
-    	var numerator = (values[half-1] + values[half]); 
+        var numerator = (values[half-1] + values[half]); 
         median =  numerator / 2.0;
     }
     return median; 
 }
 
-
-function play_game(gamedata) { 
-	// log_pre_game_data(); 
-	// calculate_net_profits(); 
-	// calculate_win_rate(); 
-	// summarize_new_game(); 
-}
-
-
 function finish_game(gamedata) { 
-	console.log("Game is finished"); 
-	gameselapsed = gameselapsed + 1; 
+    console.log("Game is finished"); 
+    log_post_game_data(gamedata); 
+    gameselapsed = gameselapsed + 1; 
 }
+
+function log_pre_game_data(gamedata) { 
+    console.log('--------------------------------------------'); 
+    console.log('Game is starting - JSON shown below'); 
+    console.log(JSON.stringify(gamedata)); 
+} 
+
+function log_post_game_data(gamedata) { 
+    console.log('--------------------------------------------'); 
+    console.log('Game is starting - JSON shown below'); 
+    console.log(JSON.stringify(gamedata)); 
+} 
+
+function calculate_win_rate() { 
+    console.log('calculating win rate'); 
+    if (numwinners > 0 && numlosers > 0) {
+        var modifier =  numwinners / numlosers; 
+        winrate = modifier*100;
+    }
+    if (numwinners > 0 && numlosers == 0){
+        winrate = 100;
+    }
+    console.log("win rate is currently " + winrate); 
+    return winrate; 
+} 
+
+function calculate_net_profits() { 
+    console.log("calculating net profits..."); 
+    profits_have_changed = grosswinnings > 0 && grosslosses > 0; 
+    var netchange = 0; 
+    if (profits_have_changed == true){
+        var oldnet = netprofits; 
+        netprofits = ((grosswinnings / grosslosses ) * 100);
+        netchange = oldnet - netprofits; 
+        console.log("net profits have changed by " + netchange + " to " + netprofits); 
+    } else { 
+        console.log('net profits have not changed'); 
+    }
+    return netprofits; 
+}
+
+function summarize() { 
+    console.log('============================================'); 
+    console.log('==> Starting game... '); 
+    console.log('==> Gross Winnings: ' + grosswinnings); 
+    console.log('==> Gross Losses: ' + grosslosses); 
+    console.log('==> Gain/Loss of ' + totalprofit); 
+    console.log('==> # of games won: ' + numwinners); 
+    console.log('==> # of games lost: ' + numlosers); 
+    console.log('==> % of games won: ' + winrate + numlosers); 
+    console.log('==> # of games elapsed: ' + gameselapsed); 
+}
+
+
+
